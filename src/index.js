a60608953cec7ae47bacc28024eaff70e2f447d3
Marco Antônio Furtado <marcofurt@gmail.com>
2021-10-04 17:08:58 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Teste from './components/Teste'
 import './index.css'
 
 ReactDOM.render(
     <div>
-      <Teste />
       <App/>
     </div>
     ,
