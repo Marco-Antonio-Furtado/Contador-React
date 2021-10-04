92444636008197317354b84e5bed83e129035c86
Marco Antônio Furtado <marcofurt@gmail.com>
2021-10-04 17:09:41 -0300

    Criando os botoes e as funções necessárias

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import "./App.css"
 
 function App() {
+  const [number, setNumber] = useState (0) 
+  
+  function menos(){
+    setNumber (number - 1)
+  } 
+  
+  function zerar(){
+    setNumber (0)
+  }
+
+  function mais(){
+    setNumber (number + 1)
+  } 
+
   return (
     <div className="App">
-     <h3>Olá mundo</h3>
-     <h4>Olaaaaaa 2</h4>
+    
+    <h1>Contador</h1>
+    <div id="numero">{number} <br />
+    <button className ="btn-menos btn" onClick= {menos} >menos</button>
+    <button className ="btn-zerar btn" onClick= {zerar}>zerar</button>
+    <button className ="btn-mais btn" onClick= {mais}>mais</button>
+    
+    </div>
+
     </div>
   );
 }
